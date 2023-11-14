156bb24c33a315de27df94a5f0fba14e80083d4d
Arunkumar <arunkumar.magudees@gmail.com>
2023-11-14 23:27:38 +0530

    folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome</div>;
+  return (
+    <>
+      <div className="App"> ODC MARKET PRICE LIST</div>
+    </>
+  );
 }
 
 export default App;
