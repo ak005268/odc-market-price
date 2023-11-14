1bef845b4118e1a383d39c854d91abf00dbcb8a5
Arunkumar <arunkumar.magudees@gmail.com>
2023-11-15 01:54:07 +0530

    tailwind and scss added

---
+++ src/assets/Image.tsx
@@ -0,0 +1,7 @@
+// import React from "react";
+import Tomato from "./images/tomato.jpg";
+import Tomato1 from "./images/tomato1.png";
+
+const Image = { Tomato, Tomato1 };
+
+export default Image;
---
+++ src/pages/Sample.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+// import yourJPGImage from "../assets/icons";
+const Sample = () => {
+  return <div>Sample</div>;
+};
+
+export default Sample;
