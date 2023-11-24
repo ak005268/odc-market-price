fde19e9337c4309aaaf2dc5fbb197ae650fbc3ec
Arunkumar K <arunsrid11@gmail.com>
2023-11-24 08:08:41 +0530

    background theme added annd scrool bar added

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,7 +1,8 @@
 import Tomato from "../assets/images/tomato.jpg";
+import Setbg from "../assets/images/setlight.jpg";
 import Tomato1 from "../assets/images/tomato1.png";
 import Logo from "../assets/icons/logo-color.png";
 
-const Image = { Tomato, Tomato1, Logo };
+const Image = { Tomato, Tomato1, Logo, Setbg };
 
 export default Image;
--- src/layouts/OuterLayout.tsx
+++ src/layouts/OuterLayout.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+// import Image from "../components/Image";
 
 const OuterLayout = () => {
   return (
-    <div className="">
+    <div className="theme-bg ">
       <Header />
       <Outlet />
     </div>
